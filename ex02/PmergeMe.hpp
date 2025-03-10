16982c5d5b246ff701716dec06e66cecbd76c2c8
zanikin <zanikin@student.42yerevan.am>
2025-03-10 16:57:02 +0400

    init

---
+++ ex02/PmergeMe.hpp
@@ -0,0 +1,9 @@
+#ifndef EX02_PMERGEME_HPP
+# define EX02_PMERGEME_HPP
+
+# include <vector>
+# include <deque>
+
+void	pmergeme(std::vector<unsigned> &vec);
+void	pmergeme(std::deque<unsigned> &deq);
+#endif
