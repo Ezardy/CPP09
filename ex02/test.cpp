35033b484f9941393ba308026a13c64d85cd8ee5
zanikin <zanikin@student.42yerevan.am>
2025-04-04 21:25:41 +0400

    non-unique tests

--- ex02/test.cpp
+++ ex02/test.cpp
@@ -12,6 +12,7 @@ static bool pmergeme_vector_4(void);
 static bool pmergeme_vector_5(void);
 static bool pmergeme_vector_6(void);
 static bool pmergeme_vector_21(void);
+static bool pmergeme_vector_nonunique(void);
 static bool pmergeme_vector_15000(void);
 
 static bool pmergeme_deque_0(void);
@@ -22,6 +23,7 @@ static bool pmergeme_deque_4(void);
 static bool pmergeme_deque_5(void);
 static bool pmergeme_deque_6(void);
 static bool pmergeme_deque_21(void);
+static bool pmergeme_deque_nonunique(void);
 static bool pmergeme_deque_15000(void);
 
 static bool is_sorted(std::vector<unsigned> const& v);
@@ -29,22 +31,35 @@ static bool is_sorted(std::deque<unsigned> const& v);
 
 struct UnsignedSequence {
 public:
-	UnsignedSequence() : a(0) {}
+	UnsignedSequence() : a(0), i(0), step(1) {}
 
-	unsigned operator()(void) { return a++; }
+	UnsignedSequence(unsigned i) : a(0), i(0), step(i) {}
+
+	unsigned operator()(void) {
+		if (i == step) {
+			i = 0;
+			a += 1;
+		} else
+			i += 1;
+		return a;
+	}
 
 private:
 	unsigned a;
+	unsigned i;
+	unsigned step;
 };
 
 int main() {
-	bool   success = true;
-	bool   (*tests[])(void) = {pmergeme_vector_0, pmergeme_vector_1,  pmergeme_vector_2,
-							   pmergeme_vector_3, pmergeme_vector_4,  pmergeme_vector_5,
-							   pmergeme_vector_6, pmergeme_vector_21, pmergeme_vector_15000,
-							   pmergeme_deque_0,  pmergeme_deque_1,	  pmergeme_deque_2,
-							   pmergeme_deque_3,  pmergeme_deque_4,	  pmergeme_deque_5,
-							   pmergeme_deque_6,  pmergeme_deque_21,  pmergeme_deque_15000};
+	bool success = true;
+	bool (*tests[])(void) = {
+		pmergeme_vector_0,		  pmergeme_vector_1,   pmergeme_vector_2,
+		pmergeme_vector_3,		  pmergeme_vector_4,   pmergeme_vector_5,
+		pmergeme_vector_6,		  pmergeme_vector_21,  pmergeme_vector_nonunique,
+		pmergeme_vector_15000,	  pmergeme_deque_0,	   pmergeme_deque_1,
+		pmergeme_deque_2,		  pmergeme_deque_3,	   pmergeme_deque_4,
+		pmergeme_deque_5,		  pmergeme_deque_6,	   pmergeme_deque_21,
+		pmergeme_deque_nonunique, pmergeme_deque_15000};
 	size_t tests_count = sizeof(tests) / sizeof(tests[0]);
 	for (size_t i = 0; success && i < tests_count; i += 1) {
 		success = tests[i]();
@@ -61,6 +76,19 @@ TEST_START(pmergeme_deque_15000)
 		std::deque<unsigned>	d(15000);
 
 		std::generate(d.begin(), d.end(), UnsignedSequence());
+		std::random_shuffle(d.begin(), d.end());
+		pmergeme(d);
+		TEST_ASSERT(is_sorted(d))
+	TEST_LOGIC_END
+	TEST_EMERGENCY_START
+	TEST_EMERGENCY_END
+TEST_END
+
+TEST_START(pmergeme_deque_nonunique)
+	TEST_LOGIC_START
+		std::deque<unsigned>	d(301);
+
+		std::generate(d.begin(), d.end(), UnsignedSequence(5));
 		std::random_shuffle(d.begin(), d.end());
 		pmergeme(d);
 		TEST_ASSERT(is_sorted(d))
@@ -188,6 +216,19 @@ TEST_START(pmergeme_vector_15000)
 		std::vector<unsigned>	v(15000);
 
 		std::generate(v.begin(), v.end(), UnsignedSequence());
+		std::random_shuffle(v.begin(), v.end());
+		pmergeme(v);
+		TEST_ASSERT(is_sorted(v))
+	TEST_LOGIC_END
+	TEST_EMERGENCY_START
+	TEST_EMERGENCY_END
+TEST_END
+
+TEST_START(pmergeme_vector_nonunique)
+	TEST_LOGIC_START
+		std::vector<unsigned>	v(301);
+
+		std::generate(v.begin(), v.end(), UnsignedSequence(5));
 		std::random_shuffle(v.begin(), v.end());
 		pmergeme(v);
 		TEST_ASSERT(is_sorted(v))
